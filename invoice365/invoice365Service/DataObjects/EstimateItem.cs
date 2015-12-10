85ba4c313d98d34a95da343c63272943f24ab756
buboodi <jamesboo@connectcode.net>
2015-12-10 13:32:37 +0800

    license

--- invoice365/invoice365Service/DataObjects/EstimateItem.cs
+++ invoice365/invoice365Service/DataObjects/EstimateItem.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.WindowsAzure.Mobile.Service;
+﻿//
+// Invoice 360
+//
+// Copyright (c) 2012-2015 invoicesoftware360.com (http://invoicesoftware360.com/GPL-LICENSE.txt)
+// Licensed under the GPL (GPL-LICENSE.txt) licenses.
+//
+// http://www.invoicesoftware360.com
+//
+//
+using Microsoft.WindowsAzure.Mobile.Service;
 
 namespace invoice365Service.DataObjects
 {
